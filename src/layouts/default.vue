b4a9d0424a9aa6a87256c40c2c1cbc3e2d04062f
Asher Lien <birdym.tw@gmail.com>
2025-11-16 15:16:31 +0800

    更新

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,24 +1,19 @@
 <script setup lang="ts">
-import { NLayout } from 'naive-ui'
 import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <div>
-    <n-layout>
-      <LayoutsHeader />
-    </n-layout>
-    <n-layout has-sider>
-      <LayoutSidebar />
-      <router-view
-        v-slot="{ Component }"
-        class="w-full"
-      >
-        <transition name="pagefade">
-          <component :is="Component" />
-        </transition>
-      </router-view>
-    </n-layout>
+  <div>>
+
+    <router-view
+      v-slot="{ Component }"
+      class="w-full"
+    >
+      <transition name="pagefade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+
   </div>
 </template>
