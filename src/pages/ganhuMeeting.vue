d228c740d38eb4c58d71b88587c5bb472c811355
Asher Lien <birdym.tw@gmail.com>
2025-11-16 15:15:55 +0800

    建立黨ㄓㄢ

---
+++ src/pages/ganhuMeeting.vue
@@ -0,0 +1,14 @@
+<route lang="yaml">
+  name: 'GangHu Meeting'
+</route>
+
+<script setup lang='ts'>
+const aaa = ref('aaaa')
+
+</script>
+
+<template>
+  <div>
+    {{ aaa }}
+  </div>
+</template>
