c8109e39700a8ccb8ec05a8203972051c1479c52
Asher Lien <birdym.tw@gmail.com>
2026-01-21 15:04:16 +0800

    更新

--- src/pages/m.vue
+++ src/pages/m.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+const data = [
+	{
+		id: '11',
+		name: 'Strensiq',
+		description: 'Strensiq',
+		pName: 'Strensiq',
+		number: 1,
+		offsetX: 10,
+		offsetY: -40
+	}, {
+		id: '2',
+		name: 'qwe',
+		description: 'qwewq qwewq',
+		pName: 'qeqwe q wewq ',
+		number: 1,
+		offsetX: 10,
+		offsetY: -40
+	}
+]
+</script>
 <template>
-  <div>mmmmm</div>
+  <div>m m mmm</div>
 </template>
