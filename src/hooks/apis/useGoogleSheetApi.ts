74efda5a39b6c7a5e9b80b7345420bfada54db01
Asher Lien <birdym.tw@gmail.com>
2026-02-04 14:44:30 +0800

    更新

--- src/hooks/apis/useGoogleSheetApi.ts
+++ src/hooks/apis/useGoogleSheetApi.ts
@@ -1,7 +1,17 @@
 import { useGoogleSheetRequest } from '../http'
 
+interface GanghuMeetingResponse {
+	time: string
+  place: string
+  title: string
+  name: string
+  offsetX: number
+  offsetY: number
+  description: string
+}
+
 export const useGoogleSheetApi = {
-  ganghuMeeting: () => useGoogleSheetRequest.get({
+  ganghuMeeting: () => useGoogleSheetRequest.get<GanghuMeetingResponse[]>({
     url: '/AKfycbzJfW01fM3a9PnlQvOufaPc3N7I2Y-nUPzJkAXZYlTr9emQZSROzQLKu-eFp9uA8axV/exec'
   })
 }
