8b3a466201dff478fdfc6a89b8e3d7b09ea64ad9
Asher Lien <birdym.tw@gmail.com>
2026-02-04 11:29:39 +0800

    wu

--- src/pages/m.vue
+++ src/pages/m.vue
@@ -392,7 +392,7 @@ const uniqueTimes = computed(() => {
 	const times = [
 		...new Set(data
 	.map(item => item.time)
-	.filter((time): time is Time => time !== undefined))]
+	.filter((time)=> time !== undefined))]
 	return times.sort()
 })
 
