48b9f0e0bc15f630d2445b53209b9dbf78abed81
Asher Lien <birdym.tw@gmail.com>
2026-05-10 23:17:37 +0800

    asd

---
+++ src/pages/wedding.vue
@@ -0,0 +1,16 @@
+<route lang="yaml">
+  name: 'Wedding'
+  meta:
+    layout: false
+</route>
+
+<script setup lang="ts">
+</script>
+
+<template>
+  <iframe
+    src="https://tide-toothpaste-9ce.notion.site/0621-35c09c69269c80b4b0c0e77fd1cd133c"
+    class="fixed inset-0 w-100vw h-100vh border-0"
+    allowfullscreen
+  />
+</template>
