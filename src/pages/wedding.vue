53965490edb75e11446404d891cf4c0cc330ec15
Asher Lien <birdym.tw@gmail.com>
2026-05-10 23:26:49 +0800

    asdsaa

--- src/pages/wedding.vue
+++ src/pages/wedding.vue
@@ -8,9 +8,7 @@
 </script>
 
 <template>
-  <iframe
-    src="https://tide-toothpaste-9ce.notion.site/0621-35c09c69269c80b4b0c0e77fd1cd133c"
-    class="fixed inset-0 w-100vw h-100vh border-0"
-    allowfullscreen
-  />
+    <h1>0621結婚聚會</h1>
+  <img src="/wedding.jpg" alt="Wedding" class="w-full h-auto" />
+
 </template>
